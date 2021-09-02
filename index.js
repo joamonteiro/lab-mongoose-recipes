cae276aaa487b018badc0d95a018c00424324390
João Monteiro <jfcm.monteiro@gmail.com>
2021-09-02 15:45:14 +0100

    Completed lab

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
+
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -20,8 +22,36 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    updateDb();
   })
   .catch(error => {
-    console.error('Error connecting to the database', error);
+    console.log('Error connecting to the database', error);
   });
+
+  const updateDb = async ()=> {
+    try{
+      const createdRecipe = await Recipe.create({
+        title: "Peanut Butter Jelly Sandwich",
+        level: "Amateur Chef",
+        ingredients: ["peanut butter", "strawberry jelly", "white bread"],
+        cuisine: "American",
+        dishType: "snack",
+        image: "https://i.insider.com/605e438d8f71c3001853a9a0?width=700",
+        duration: 5,
+        creator: " Julia Davis Chandler",
+      })
+      console.log(createdRecipe.title);
+      
+      await Recipe.insertMany(data);
+  
+      await data.forEach(recipe => {
+        console.log(recipe.title);
+      });
+  
+      await Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, console.log("Sucess!"))
+    
+      await Recipe.deleteOne({title: "Carrot Cake"}, console.log("Delete Sucess!"))
+    } finally {
+      mongoose.connection.close();
+    }
+  }
